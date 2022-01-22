2bba2097e3bc0dc4d5d647f4622734bd2bf43f80
Fawzaan <fawzi20112001@gmail.com>
2022-01-22 16:48:16 +0530

    JS Practice added

---
+++ practice.js
@@ -0,0 +1,27 @@
+// This is the practice JavaScript file
+
+// Everything related to Operators, Variables, etc. are the same as other languages
+// Datatypes -> number, string, boolean, symbol, float, ...
+
+
+function avg(a, b) {
+    return (a + b) / 2;
+}
+
+c = avg(2, 3);
+
+console.log(c);
+
+// Working with arrays
+var arr = [1, 2, 3, 4, 5, 6, 7];
+arr.forEach(function(element) {
+    console.log(element);   // This will print out all the elements of the array
+});
+
+let j = 0; // let is a block-level variable allocation keyword. This helps in memory management as it is not retained after the scope is over.
+
+arr.push("something"); // push() method pushes an element to the end of the array
+arr.pop(); // pop() method removes the last element of the array
+
+
+
