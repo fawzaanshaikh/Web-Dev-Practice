2fee28373caed8d15ad3d2080e0615f548d9e425
Fawzaan <fawzi20112001@gmail.com>
2022-02-05 16:29:45 +0530

    CodeWithHarry JS reference complete

--- practice.js
+++ practice.js
@@ -3,7 +3,7 @@
 // Everything related to Operators, Variables, etc. are the same as other languages
 // Datatypes -> number, string, boolean, symbol, float, ...
 
-
+/* -------------------------------------------- */
 function avg(a, b) {
     return (a + b) / 2;
 }
@@ -12,6 +12,7 @@ c = avg(2, 3);
 
 // console.log(c);
 
+/* -------------------------------------------- */
 // Working with arrays
 var arr = [1, 2, 3, 4, 5, 6, 7];
 arr.forEach(function(element) {
@@ -23,10 +24,11 @@ let j = 0; // let is a block-level variable allocation keyword. This helps in me
 arr.push("something"); // push() method pushes an element to the end of the array
 arr.pop(); // pop() method removes the last element of the array
 
-
+/* -------------------------------------------- */
 // Dates
 let myDate = new Date(); // getTime, getFullYear, getMinutes, getHours, getDay
 
+/* -------------------------------------------- */
 // DOM Manipulation
 let elemClass = document.getElementsByClassName("div-container");
 // elemClass[0].classList.add("bg-change-blue");
@@ -40,6 +42,7 @@ var addChild = document.createElement('p');
 addChild.innerHTML = "This is an added para";
 elemClass[0].appendChild(addChild);
 
+/* -------------------------------------------- */
 // Events in JavaScript
 function clicked() {
     formDiv = document.getElementById("form");
@@ -55,10 +58,11 @@ elemClass[0].addEventListener("click", function() {
     elemClass[0].appendChild(addChild);
 })
 
+/* -------------------------------------------- */
 // setTimeout and setInterval
 setTimeout(function() { // setTimeout does the work after the specified time
     console.log("I am displayed after 3 seconds or 3000 milliseconds");
-}, 3000); // To cancel a setTimeout, you can use clearTimeout() and pass in teh value that is returned from setTimeout as shown below
+}, 3000); // To cancel a setTimeout, you can use clearTimeout() and pass in the value that is returned from setTimeout as shown below
 
 let iteration = 0;
 
@@ -70,3 +74,26 @@ clr = setInterval(function() {  // setInterval keeps iterating per amount of tim
     if (iteration === 5) clearInterval(clr);    // Stops the running setInterval
 }, 2000);
 
+/* -------------------------------------------- */
+// localStorage in JavaScript
+// localStorage.setItem("name", "Fawzaan");
+// localStorage;
+// localStorage.getItem("name");
+// localStorage.removeItem("name");
+// localStorage.clear();
+
+/* -------------------------------------------- */
+// To convert JS object to JSON format:
+obj = {id: "101", firstName: "Fawzaan", length: 7};
+jsonString = JSON.stringify(obj);
+console.log(jsonString);
+console.log(typeof jsonString);
+
+// For parsing
+parsed = JSON.parse(`{"id": "101", "firstName": "Fawzaan", "length": 7}`);
+console.log(parsed);
+
+/* -------------------------------------------- */
+// Template literals - Backticks
+a = 34;
+console.log(`Here, a is ${a}`); 
